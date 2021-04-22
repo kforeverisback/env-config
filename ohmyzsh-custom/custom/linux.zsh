d41974ee372b86eca108513a329c976d653220f5
Kushal Azim Ekram <kforeverisback@gmail.com>
2021-04-21 19:34:16 -0500

    Updated

--- ohmyzsh-custom/custom/linux.zsh
+++ ohmyzsh-custom/custom/linux.zsh
@@ -1,5 +1,6 @@
 # Kushal: For OpenSSH Agent on WSL: https://esc.sh/blog/ssh-agent-windows10-wsl2/
 # https://blog.kylemanna.com/linux/use-funtoos-keyhain-insetad-of-gnome-keyring/
+alias rgv='rg -v "rg " | rg'
 update_keychain() {
     if ! command -v keychain &> /dev/null; then
       echo "'keychain' not found in system."
@@ -7,7 +8,7 @@ update_keychain() {
       echo 'Starting ssh-agent via keychain'
       /usr/bin/keychain -q --nogui --timeout 180 $HOME/.ssh/id_rsa $HOME/.ssh/github-key-ms
       source $HOME/.keychain/$HOST-sh
-      _k_help+="Use update_keychain to add keys to ssh-agent"
     fi
 }
+_k_help+="Use update_keychain to add keys to ssh-agent"
 update_keychain
