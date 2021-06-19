077992ebe723eb9d550645fcf774a3450a8c74f9
Kushal Azim Ekram <kforeverisback@gmail.com>
2021-06-19 00:25:49 -0500

    Aliased xxrm to /bin/rm.

--- ohmyzsh-custom/custom/shell_utils.zsh
+++ ohmyzsh-custom/custom/shell_utils.zsh
@@ -3,7 +3,8 @@
 # Built 'cp' from scratch with Advanced mod: https://github.com/jarun/advcpmvn
 alias cp='cp_adv_mod_8.32 -gi'
 alias mv='mv_adv_mod_8.32 -gi'
-alias rm="echo Use del/trash, or the full path i.e. '/bin/rm'"
+alias rm="echo Use del/trash. /bin/rm is aliased to xxrm"
+alias xxrm='/bin/rm'
 alias grm="echo Use del/trash, or the full path i.e. '/usr/local/bin/grm'"
 # If you're having SpaceVim issues with NVim, disable nvim aliases
 alias vim='nvim'
