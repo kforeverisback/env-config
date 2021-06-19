8a877959b47a356b5cf20744cd5bd6c53b8aae52
Kushal Azim Ekram <mekram@microsoft.com>
2021-06-18 19:07:10 -0500

    Added timeout to update_keychain function

--- ohmyzsh-custom/custom/linux.zsh
+++ ohmyzsh-custom/custom/linux.zsh
@@ -2,12 +2,19 @@
 # https://blog.kylemanna.com/linux/use-funtoos-keyhain-insetad-of-gnome-keyring/
 alias rgv='rg -v "rg " | rg'
 update_keychain() {
-    if ! command -v keychain &> /dev/null; then
-      echo "'keychain' not found in system."
-    else
-      echo 'Starting ssh-agent via keychain'
-      /usr/bin/keychain -q --nogui --timeout 180 $HOME/.ssh/id_rsa $HOME/.ssh/github-key-ms
-      source $HOME/.keychain/$HOST-sh
-    fi
+  if ! command -v keychain &> /dev/null; then
+    echo "'keychain' not found in system."
+  else
+    timeout=180 # Default
+    [[ -z "$1" ]] || timeout=$1
+    echo 'Starting ssh-agent via keychain'
+    echo SSH-Agent timeout: ${timeout} minute(s)
+    #if [[ $(pgrep -a ssh-agent | sed 's/^.*-t //g') -ne $(( timeout * 60 )) ]];
+    #then
+    #  keychain -q -k all
+    #fi
+    keychain -q --nogui --timeout ${timeout} $HOME/.ssh/id_rsa $HOME/.ssh/github-key-ms
+    source $HOME/.keychain/$HOST-sh
+  fi
 }
 _k_help+="Use update_keychain to add keys to ssh-agent"
--- ohmyzsh-custom/custom/wsl2.zsh
+++ ohmyzsh-custom/custom/wsl2.zsh
@@ -16,15 +16,22 @@ export LIBGL_ALWAYS_INDIRECT=1
 # Kushal: For OpenSSH Agent on WSL: https://esc.sh/blog/ssh-agent-windows10-wsl2/
 # https://blog.kylemanna.com/linux/use-funtoos-keyhain-insetad-of-gnome-keyring/
 update_keychain() {
-    if ! command -v keychain &> /dev/null; then
-      echo "'keychain' not found in system."
-    else
-      echo 'Starting ssh-agent via keychain'
-      /usr/bin/keychain -q --nogui --timeout 180 $HOME/.ssh/id_rsa $HOME/.ssh/github-key-ms
-      source $HOME/.keychain/$HOST-sh
-      _k_help+="Use update_keychain to add keys to ssh-agent"
-    fi
+  if ! command -v keychain &> /dev/null; then
+    echo "'keychain' not found in system."
+  else
+    timeout=180 # Default
+    [[ -z "$1" ]] || timeout=$1
+    echo 'Starting ssh-agent via keychain'
+    echo SSH-Agent timeout: ${timeout} minute(s).
+    #if [[ $(pgrep -a ssh-agent | sed 's/^.*-t //g') -ne $(( timeout * 60 )) ]];
+    #then
+    #  keychain -q -k all
+    #fi
+    keychain -q --nogui --timeout ${timeout} $HOME/.ssh/id_rsa $HOME/.ssh/github-key-ms
+    source $HOME/.keychain/$HOST-sh
+  fi
 }
+_k_help+="Use update_keychain to add keys to ssh-agent"
 update_keychain
 
 # Sourcing Azure's Bash completion
