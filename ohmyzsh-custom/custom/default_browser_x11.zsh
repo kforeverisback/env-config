0931315b440ed513eb22067173ab29bf2b7ef429
Kushal Azim Ekram <mekram@microsoft.com>
2022-01-22 11:00:41 -0600

    Moved default browser to edge and added touchpad configuration

--- ohmyzsh-custom/custom/default_browser_x11.zsh
+++ ohmyzsh-custom/custom/default_browser_x11.zsh
@@ -1,3 +1,4 @@
 xdg-mime default firefox.desktop x-scheme-handler/http
 xdg-mime default firefox.desktop x-scheme-handler/https
-export BROWSER=/usr/bin/firefox
+#export BROWSER=/usr/bin/firefox
+export BROWSER=/usr/bin/microsoft-edge-stable
---
+++ touchpad-config/README.md
@@ -0,0 +1,4 @@
+# How to use
+
+- Copy the 30-touchpad.conf to /etc/X11/xorg.conf.d/
+- Logoff or reboot for natual scroll
