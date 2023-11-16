a50ee4ef82f61719d4261b3c91adc6d6cd5ee088
Kushal Azim Ekram <kforeverisback@gmail.com>
2023-11-16 02:42:06 -0800

    Updated cred manager

--- ffmpeg-scripts/download-multiple-m3u8-segments.bash
+++ ffmpeg-scripts/download-multiple-m3u8-segments.bash
@@ -1,26 +1,35 @@
 #!/bin/bash
 
 usage () {
-    echo "$0 <URL_FILE> <INDEX> <OUT_DIR>"
-    echo "URL File should consist of a list of index.m3u8 links"
+    echo "$0 <URL_FILE> <FILE_INDEX> <OUT_DIR>"
+    echo "Params:
+    URL_FILE    - file containing list of index.m3u8 links
+    FILE_INDEX  - starting file index to download in 'URL_FILE'
+    OUT_DIR     - output directory to download m3u8 segments to.
+    "
+    echo "Params can also be specified using environment variables of the same name."
 }
 
 URL_FILE=$1
-INDEX_START=$2
+FILE_INDEX=$2
 OUT_DIR=$3
 
-[[ -z "$URL_FILE" || ! -f "$URL_FILE" ]] && >&2 echo "URL file not found." && usage && exit 1
-[[ -z "$INDEX_START" ]] && INDEX_START=1
+[[ -z "$URL_FILE" || ! -f "$URL_FILE" ]] && >&2 echo "'m3u8' URL file not found." && usage && exit 1
+[[ -z "$FILE_INDEX" ]] && FILE_INDEX=1
 [[ -z "$OUT_DIR" ]] && OUT_DIR="."
 
 echo "Downloading TS files to: $OUT_DIR"
-echo "Starting index: $INDEX_START"
+echo "Starting index: $FILE_INDEX"
 sleep 3
+
+if [[ -n "$NO_VERIFY_URL_FILE" ]]; then
+    echo Aria2c will not verify the URL file.
+fi
 
 while IFS= read -r line
 do
     [[ -z "$line" ]] && continue
-    dir="$OUT_DIR/E$(printf '%02d' "$INDEX_START")"
+    dir="$OUT_DIR/E$(printf '%02d' "$FILE_INDEX")"
     echo "Downloading: $line"
     mkdir -p "$dir"
     pushd "$dir" || (echo "pushd error" && exit)
@@ -28,8 +37,9 @@ do
     basefile=$(basename "$line")
     url=$(echo "$line" | sed "s/$basefile/{}/g")
     grep -v '#' "$basefile" | xargs -n1 -I{} echo "$url" > input
+    # aria2c will download multiple files in parallel from "input" file, but it will only download one segment at a time
     # aria2c params: https://stackoverflow.com/questions/55166245/aria2c-parallel-download-parameters
-    aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --auto-file-renaming=false -i input
-    INDEX_START=$((INDEX_START+1))
+    aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --auto-file-renaming=false -i "input"
+    FILE_INDEX=$((FILE_INDEX+1))
     popd || (echo "popd error" && exit)
 done < <(cat "$URL_FILE")
