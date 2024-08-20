0e53b96fda1433f8fd70c0f1030d6be8ce9d4cdb
Kushal Azim Ekram <mekram@microsoft.com>
2024-08-20 01:22:58 -0700

    Updated zshrc, ffmpeg scripts

--- ffmpeg-scripts/download-m3u8-segments.bash
+++ ffmpeg-scripts/download-m3u8-segments.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+shopt -os nounset pipefail errexit errtrace
 usage() {
-	echo "$0 <INDEX_URL> <OUT_DIR> <OUT_FFMPEG_FILE>
+  echo "$0 <INDEX_URL> <OUT_FFMPEG_FILE> [OUT_DIR]
 Params:
   INDEX_URL   - URL pointing to an m3u8 file.
                 The link would serve as a base URL for downloading segments,
@@ -9,22 +9,21 @@ Params:
   OUT_FFMPEG_FILE  - If set, in addition to downloading segment files,
                 an OUT_FFMPEG_FILE file will be created from the segments.
   OUT_DIR     - output directory to download m3u8 segments to.
+  CONCURRENT_DOWNLOAD - number of concurrent download. default: 2
   "
-	echo "Params can also be specified using environment variables of the same name."
+  echo "Params can also be specified using environment variables of the same name."
 }
 
-INDEX_URL=$1
-OUT_DIR=$2
-OUT_FFMPEG_FILE=$3
+INDEX_URL="${1:-$INDEX_URL}"
+OUT_DIR="${3:-$OUT_DIR}"
+OUT_FFMPEG_FILE="${2:-$OUT_FFMPEG_FILE}"
+CONCURRENT_DOWNLOAD="${CONCURRENT_DOWNLOAD:-2}"
 
+# No quotes on regex in a script!!
+[[ $OUT_FFMPEG_FILE =~ .*\.(mp4|mkv) ]] || (>&2 echo "Specify an output file with mp4|mkv extension" && exit 1)
 [[ -z "$INDEX_URL" ]] && >&2 echo "'m3u8' URL not provided." && usage && exit 1
 [[ -z "$OUT_DIR" ]] && OUT_DIR="."
-
 echo "Downloading TS/segment files to: $OUT_DIR"
-
-if [[ -n "$NO_VERIFY_URL_FILE" ]]; then
-	echo Aria2c will not verify the URL file.
-fi
 
 orig_index_file="aria2c-orig.m3u8"
 mod_index_file="${orig_index_file/orig/mod}"
@@ -33,7 +32,7 @@ aria_dwnld_list="aria2c.download.list"
 #mod_index_file="${orig_index_file/orig/mod}"
 #aria_dwnld_list="aria2c.list.$RANDOM"
 echo "Following files will be created/replaced:
-   orig m3u8: $orig_index_file
+   orig m3u8: $OUT_DIR/$orig_index_file
     mod m3u8: $OUT_DIR/$mod_index_file
   dwnld list: $OUT_DIR/$aria_dwnld_list"
 [[ -n "$OUT_FFMPEG" ]] && echo "FFmpeg output file: $OUT_FFMPEG"
@@ -52,36 +51,35 @@ echo "Base m3u8 Url: ${base_url}"
 valid_url_regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
 
 while IFS= read -r line; do
-	[[ -z "$line" ]] && continue
-	if (grep -oE '^#.*' <<<"$line" >/dev/null); then
-		# Its a # EXT line, not real segment link
-		echo "$line" >>"$OUT_DIR/$mod_index_file"
-		continue
-	else
-		# Its a segment link/name
-		# Now check if its a full URL
-		if [[ $line =~ $valid_url_regex ]]; then
-			# echo "Link valid"
-			# only output the base-file-name
-			segment_name_out="$(basename "$line")"
-			segment_url="$line"
-		else
-			# Its not a full url, but just a filename
-			segment_name_out="$line"
-			segment_url="${base_url}/${line}"
-		fi
-		echo "$segment_name_out" >>"$OUT_DIR/$mod_index_file"
-		echo "$segment_url" >>"$OUT_DIR/$aria_dwnld_list"
-	fi
-	#continue
+  [[ -z "$line" ]] && continue
+  if (grep -oE '^#.*' <<<"$line" >/dev/null); then
+    # Its a # EXT line, not real segment link
+    echo "$line" >>"$OUT_DIR/$mod_index_file"
+    continue
+  else
+    # Its a segment link/name
+    # Now check if its a full URL
+    if [[ $line =~ $valid_url_regex ]]; then
+      # echo "Link valid"
+      # only output the base-file-name
+      segment_name_out="$(basename "$line")"
+      segment_url="$line"
+    else
+      # Its not a full url, but just a filename
+      segment_name_out="$line"
+      segment_url="${base_url}/${line}"
+    fi
+    echo "$segment_name_out" >>"$OUT_DIR/$mod_index_file"
+    echo "$segment_url" >>"$OUT_DIR/$aria_dwnld_list"
+  fi
+  #continue
 done < <(cat "$OUT_DIR/$orig_index_file")
 
 # aria2c will download multiple files in parallel from "input" file, but it will only download one segment at a time
 # aria2c params: https://stackoverflow.com/questions/55166245/aria2c-parallel-download-parameters
-aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --console-log-level=warn --auto-file-renaming=false -d "$OUT_DIR" -i "$OUT_DIR/$aria_dwnld_list"
+aria2c -Z -c -s 1 -j "$CONCURRENT_DOWNLOAD" -x 1 -k 1M --console-log-level=error --download-result=hide --auto-file-renaming=false -d "$OUT_DIR" -i "$OUT_DIR/$aria_dwnld_list"
 
 if [[ -n "$OUT_FFMPEG_FILE" ]]; then
-	echo "FFmpeg output file: $OUT_FFMPEG_FILE"
-	ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -allowed_extensions ALL -i "$OUT_DIR/$mod_index_file" -c copy "$OUT_FFMPEG_FILE"
+  echo "FFmpeg output file: $OUT_FFMPEG_FILE"
+  ffmpeg -hide_banner -loglevel error -stats -protocol_whitelist file,http,https,tcp,tls,crypto -allowed_extensions ALL -i "$OUT_DIR/$mod_index_file" -c copy "$OUT_FFMPEG_FILE"
 fi
-
--- ffmpeg-scripts/download-multiple-m3u8-segments.bash
+++ ffmpeg-scripts/download-multiple-m3u8-segments.bash
@@ -1,45 +1,54 @@
 #!/bin/bash
-
-usage () {
-    echo "$0 <URL_FILE> <FILE_INDEX> <OUT_DIR>"
-    echo "Params:
-    URL_FILE    - file containing list of index.m3u8 links
-    FILE_INDEX  - starting file index to download in 'URL_FILE'
-    OUT_DIR     - output directory to download m3u8 segments to.
+shopt -os nounset pipefail errexit errtrace
+__DEFAULT_FILE_INDEX=1
+__DEFAULT_FILE_OUT_DIR="/var/tmp"
+__DEFAULT_SLEEP_IN_BETWEEN=60
+usage() {
+  echo "$0 <URL_FILE> <FILE_NAME_PREFIX> [SLEEP_IN_BETWEEN] [FILE_INDEX] [FILE_OUT_DIR]"
+  echo "Params:
+    URL_FILE    - file containing list of m3u8 links (required)
+    FILE_NAME_PREFIX - output name prefix (required)
+    FILE_INDEX  - starting file index to download in 'URL_FILE' (default: $__DEFAULT_FILE_INDEX)
+    FILE_OUT_DIR     - output directory to download m3u8 segments to (default: $__DEFAULT_FILE_OUT_DIR).
+    SLEEP_IN_BETWEEN - Sleep (sec) in between each link download (default: $__DEFAULT_SLEEP_IN_BETWEEN seconds)
     "
-    echo "Params can also be specified using environment variables of the same name."
+  echo "Params can also be specified using environment variables of the same name."
 }
 
-URL_FILE=$1
-FILE_INDEX=$2
-OUT_DIR=$3
+URL_FILE="$1"
+FILE_NAME_PREFIX="$2"
+FILE_INDEX="$3"
+FILE_OUT_DIR="$4"
+
+[[ -z "$FILE_INDEX" ]] && FILE_INDEX="$__DEFAULT_FILE_INDEX"
+[[ -z "$SLEEP_IN_BETWEEN" ]] && SLEEP_IN_BETWEEN="$__DEFAULT_SLEEP_IN_BETWEEN"
+[[ -z "$FILE_OUT_DIR" ]] && FILE_OUT_DIR="$__DEFAULT_FILE_OUT_DIR"
+_script_dir=$(dirname $0)
 
 [[ -z "$URL_FILE" || ! -f "$URL_FILE" ]] && >&2 echo "'m3u8' URL file not found." && usage && exit 1
-[[ -z "$FILE_INDEX" ]] && FILE_INDEX=1
-[[ -z "$OUT_DIR" ]] && OUT_DIR="."
+[[ -z "$FILE_NAME_PREFIX" ]] && >&2 echo "Provide a file name prefix for output" && usage && exit 1
 
-echo "Downloading TS files to: $OUT_DIR"
+echo "URL list: $URL_FILE"
+echo "Each m3u8 file output prefix: $FILE_NAME_PREFIX"
 echo "Starting index: $FILE_INDEX"
-sleep 3
+#sleep 3
 
 if [[ -n "$NO_VERIFY_URL_FILE" ]]; then
-    echo Aria2c will not verify the URL file.
+  echo Aria2c will not verify the URL file.
 fi
 
-while IFS= read -r line
-do
-    [[ -z "$line" ]] && continue
-    dir="$OUT_DIR/E$(printf '%02d' "$FILE_INDEX")"
-    echo "Downloading: $line"
-    mkdir -p "$dir"
-    pushd "$dir" || (echo "pushd error" && exit)
-    aria2c "$line"
-    basefile=$(basename "$line")
-    url=$(echo "$line" | sed "s/$basefile/{}/g")
-    grep -v '#' "$basefile" | xargs -n1 -I{} echo "$url" > input
-    # aria2c will download multiple files in parallel from "input" file, but it will only download one segment at a time
-    # aria2c params: https://stackoverflow.com/questions/55166245/aria2c-parallel-download-parameters
-    aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --auto-file-renaming=false -i "input"
-    FILE_INDEX=$((FILE_INDEX+1))
-    popd || (echo "popd error" && exit)
-done < <(cat "$URL_FILE")
+declare -a urls=($(cat "$URL_FILE"))
+for line in "${urls[@]}"; do
+  [[ -z "$line" ]] && continue
+  filename="$FILE_NAME_PREFIX.E$(printf '%02d' "$FILE_INDEX")"
+  output_ext="mp4"
+  dir="$FILE_OUT_DIR/$filename"
+  echo "Downloading: $line"
+  echo "  to $dir"
+  export INDEX_URL="$line"
+  export OUT_DIR="$dir"
+  export OUT_FFMPEG_FILE="${filename}.${output_ext}"
+  "$_script_dir/download-m3u8-segments.bash"
+  FILE_INDEX=$((FILE_INDEX + 1))
+  echo "Sleeping for $SLEEP_IN_BETWEEN seconds" && sleep "$SLEEP_IN_BETWEEN"
+done
