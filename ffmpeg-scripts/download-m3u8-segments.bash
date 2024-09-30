2f738aaa8671391c221bb16aabc5b59b5c352df3
Kushal Azim Ekram <kforeverisback@gmail.com>
2024-09-30 01:42:43 -0700

    Finally mulitple files downlod is working now YESS!

--- ffmpeg-scripts/download-m3u8-segments.bash
+++ ffmpeg-scripts/download-m3u8-segments.bash
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 usage() {
-	echo "$0 <INDEX_URL> <OUT_DIR> <OUT_FFMPEG_FILE>
+	echo "$0 <INDEX_URL> <OUT_FILE> <SEGMENTS_OUT_DIR> <RM_SEGMENTS>
 Params:
   INDEX_URL   - URL pointing to an m3u8 file.
                 The link would serve as a base URL for downloading segments,
                 unless the segments have full url
-  OUT_FFMPEG_FILE  - If set, in addition to downloading segment files,
-                an OUT_FFMPEG_FILE file will be created from the segments.
-  OUT_DIR     - output directory to download m3u8 segments to.
-  "
+  OUT_FILE    - If set, in addition to downloading segment files,
+                an OUT_FILE file will be created from the segments.
+  SEGMENTS_OUT_DIR     - output directory to download m3u8 segments to.
+				Default: /tmp/segments-\$RANDOM.
+  RM_SEGMENTS - If set, will remove the downloaded segments after creating OUT_FILE.
+				Default: not set.
+"
 	echo "Params can also be specified using environment variables of the same name."
 }
 
-INDEX_URL=$1
-OUT_DIR=$2
-OUT_FFMPEG_FILE=$3
+INDEX_URL="${1:-$INDEX_URL}"
+OUT_FILE="${2:-$OUT_FILE}"
+SEGMENTS_OUT_DIR="${3:-$SEGMENTS_OUT_DIR}"
+RM_SEGMENTS="${4:-$RM_SEGMENTS}"
 
 [[ -z "$INDEX_URL" ]] && >&2 echo "'m3u8' URL not provided." && usage && exit 1
-[[ -z "$OUT_DIR" ]] && OUT_DIR="."
-
-echo "Downloading TS/segment files to: $OUT_DIR"
-
-if [[ -n "$NO_VERIFY_URL_FILE" ]]; then
-	echo Aria2c will not verify the URL file.
-fi
+[[ -z "$SEGMENTS_OUT_DIR" ]] && SEGMENTS_OUT_DIR="/tmp/segments-$(basename $OUT_FILE)-$RANDOM"
 
 orig_index_file="aria2c-orig.m3u8"
 mod_index_file="${orig_index_file/orig/mod}"
@@ -32,21 +30,24 @@ aria_dwnld_list="aria2c.download.list"
 #orig_index_file="aria2c-orig-$RANDOM.m3u8"
 #mod_index_file="${orig_index_file/orig/mod}"
 #aria_dwnld_list="aria2c.list.$RANDOM"
-echo "Following files will be created/replaced:
-   orig m3u8: $orig_index_file
-    mod m3u8: $OUT_DIR/$mod_index_file
-  dwnld list: $OUT_DIR/$aria_dwnld_list"
-[[ -n "$OUT_FFMPEG" ]] && echo "FFmpeg output file: $OUT_FFMPEG"
+# echo "Following files will be created/replaced:
+#    orig m3u8: $orig_index_file
+#     mod m3u8: $SEGMENTS_OUT_DIR/$mod_index_file
+#   dwnld list: $SEGMENTS_OUT_DIR/$aria_dwnld_list"
+echo "Downloading segment files to: $SEGMENTS_OUT_DIR"
+echo "Output file: $OUT_FILE"
 
 sleep 3
-/bin/rm -f "$OUT_DIR/$orig_index_file"
-/bin/rm -f "$OUT_DIR/$mod_index_file"
-/bin/rm -f "$OUT_DIR/$aria_dwnld_list"
+/bin/rm -f "$SEGMENTS_OUT_DIR/$orig_index_file"
+/bin/rm -f "$SEGMENTS_OUT_DIR/$mod_index_file"
+/bin/rm -f "$SEGMENTS_OUT_DIR/$aria_dwnld_list"
+set -e
+mkdir -p "$SEGMENTS_OUT_DIR"
+mkdir -p "$(dirname "$OUT_FILE")"
 #aria2c -d "$(dirname $tmp_index_file)" "\"$INDEX_URL\"" --out="$(basename $tmp_index_file)"
-echo aria2c "$INDEX_URL" -d "$OUT_DIR" --out="$orig_index_file"
-mkdir -p "$OUT_DIR"
-aria2c "$INDEX_URL" -d "$OUT_DIR" --out="$orig_index_file"
-
+# echo aria2c "$INDEX_URL" -d "$SEGMENTS_OUT_DIR" --out="$orig_index_file"
+aria2c "$INDEX_URL" -d "$SEGMENTS_OUT_DIR" --console-log-level=warn --out="$orig_index_file" --download-result=hide
+echo
 base_url=$(dirname "$INDEX_URL")
 echo "Base m3u8 Url: ${base_url}"
 valid_url_regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
@@ -55,7 +56,7 @@ while IFS= read -r line; do
 	[[ -z "$line" ]] && continue
 	if (grep -oE '^#.*' <<<"$line" >/dev/null); then
 		# Its a # EXT line, not real segment link
-		echo "$line" >>"$OUT_DIR/$mod_index_file"
+		echo "$line" >>"$SEGMENTS_OUT_DIR/$mod_index_file"
 		continue
 	else
 		# Its a segment link/name
@@ -70,18 +71,23 @@ while IFS= read -r line; do
 			segment_name_out="$line"
 			segment_url="${base_url}/${line}"
 		fi
-		echo "$segment_name_out" >>"$OUT_DIR/$mod_index_file"
-		echo "$segment_url" >>"$OUT_DIR/$aria_dwnld_list"
+		echo "$segment_name_out" >>"$SEGMENTS_OUT_DIR/$mod_index_file"
+		echo "$segment_url" >>"$SEGMENTS_OUT_DIR/$aria_dwnld_list"
 	fi
 	#continue
-done < <(cat "$OUT_DIR/$orig_index_file")
+done < <(cat "$SEGMENTS_OUT_DIR/$orig_index_file")
 
 # aria2c will download multiple files in parallel from "input" file, but it will only download one segment at a time
 # aria2c params: https://stackoverflow.com/questions/55166245/aria2c-parallel-download-parameters
-aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --console-log-level=warn --auto-file-renaming=false -d "$OUT_DIR" -i "$OUT_DIR/$aria_dwnld_list"
+aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --console-log-level=warn --auto-file-renaming=false -d "$SEGMENTS_OUT_DIR" -i "$SEGMENTS_OUT_DIR/$aria_dwnld_list" --download-result=hide
+echo
+if [[ -n "$OUT_FILE" ]]; then
+	echo "Running FFmpeg (output: $OUT_FILE)"
+	ffmpeg -hide_banner -loglevel warn -protocol_whitelist file,http,https,tcp,tls,crypto -allowed_extensions ALL -i "$SEGMENTS_OUT_DIR/$mod_index_file" -c copy "$OUT_FILE"
+fi
 
-if [[ -n "$OUT_FFMPEG_FILE" ]]; then
-	echo "FFmpeg output file: $OUT_FFMPEG_FILE"
-	ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -allowed_extensions ALL -i "$OUT_DIR/$mod_index_file" -c copy "$OUT_FFMPEG_FILE"
+if [[ -n "$RM_SEGMENTS" ]]; then
+	echo "Removing $SEGMENTS_OUT_DIR"
+	/bin/rm -rf "$SEGMENTS_OUT_DIR"
 fi
 
--- ffmpeg-scripts/download-multiple-m3u8-segments.bash
+++ ffmpeg-scripts/download-multiple-m3u8-segments.bash
@@ -1,45 +1,46 @@
 #!/bin/bash
 
 usage () {
-    echo "$0 <URL_FILE> <FILE_INDEX> <OUT_DIR>"
+    echo "$0 <URL_FILE> <OUT_DIR> <FILE_INDEX> <NAME_PREFIX>"
     echo "Params:
     URL_FILE    - file containing list of index.m3u8 links
-    FILE_INDEX  - starting file index to download in 'URL_FILE'
-    OUT_DIR     - output directory to download m3u8 segments to.
+    OUT_DIR     - output directory for mp4 files.
+    FILE_INDEX  - starting index value (default: 1)
+    NAME_PREFIX - Prefix of each file name (deafult: E).
     "
     echo "Params can also be specified using environment variables of the same name."
 }
 
-URL_FILE=$1
-FILE_INDEX=$2
-OUT_DIR=$3
+# DEBUGGING
+# /bin/rm -rf ./output
+# /bin/rm -rf /tmp/segments-*
+
+URL_FILE=${1:-urls}
+OUT_DIR=${2:-output}
+FILE_INDEX=${3:-1}
+NAME_PREFIX=${4:-E}
+
+[[ -z "$URL_FILE" ]] && >&2 echo "'m3u8' URL file not provided." && usage && exit 1
 
 [[ -z "$URL_FILE" || ! -f "$URL_FILE" ]] && >&2 echo "'m3u8' URL file not found." && usage && exit 1
 [[ -z "$FILE_INDEX" ]] && FILE_INDEX=1
 [[ -z "$OUT_DIR" ]] && OUT_DIR="."
 
-echo "Downloading TS files to: $OUT_DIR"
-echo "Starting index: $FILE_INDEX"
+echo "Downloading files to : $OUT_DIR"
+echo "Starting index       : $FILE_INDEX"
 sleep 3
 
-if [[ -n "$NO_VERIFY_URL_FILE" ]]; then
-    echo Aria2c will not verify the URL file.
-fi
-
+set -e
+# for line in $(cat urls);do
+# done
 while IFS= read -r line
 do
     [[ -z "$line" ]] && continue
-    dir="$OUT_DIR/E$(printf '%02d' "$FILE_INDEX")"
-    echo "Downloading: $line"
-    mkdir -p "$dir"
-    pushd "$dir" || (echo "pushd error" && exit)
-    aria2c "$line"
-    basefile=$(basename "$line")
-    url=$(echo "$line" | sed "s/$basefile/{}/g")
-    grep -v '#' "$basefile" | xargs -n1 -I{} echo "$url" > input
-    # aria2c will download multiple files in parallel from "input" file, but it will only download one segment at a time
-    # aria2c params: https://stackoverflow.com/questions/55166245/aria2c-parallel-download-parameters
-    aria2c -Z -c -s 1 -j 5 -x 1 -k 1M --auto-file-renaming=false -i "input"
+    OUT_FILE="$OUT_DIR/${NAME_PREFIX}$(printf '%02d' "$FILE_INDEX").mp4"
+    export OUT_FILE
+    export INDEX_URL="$line"
+    export RM_SEGMENTS="YES"
+    echo "  m3u8: $INDEX_URL, output: $OUT_FILE"
+    bash download-m3u8-segments.bash
     FILE_INDEX=$((FILE_INDEX+1))
-    popd || (echo "popd error" && exit)
-done < <(cat "$URL_FILE")
+done< <(cat urls)
