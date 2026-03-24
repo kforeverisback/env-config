bbdd38159ffb53cc295466cab2c7fc8c2aa8666d
Kushal Azim Ekram <11367790+kforeverisback@users.noreply.github.com>
2026-03-24 12:54:06 -0700

    feat(end4-hyprland): Added end4 custom config

---
+++ end4-hyprland/custom/scripts/pre_workspace_action.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+curr_workspace="$(hyprctl activeworkspace -j | jq -r ".id")"
+
+dispatcher="$1"
+shift ## The target is now in $1, not $2
+
+if [[ -z "${dispatcher}" || "${dispatcher}" == "--help" || "${dispatcher}" == "-h" || -z "$1" ]]; then
+  echo "Usage: $0 <dispatcher> <target>"
+  exit 1
+fi
+if [[ "${curr_workspace}" -eq "$1" ]]; then
+  hyprctl dispatch workspace previous
+else
+    # otherwise, switch to the target workspace with standard script from ~/.config/hypr/hyprland/keybinds.conf
+    exec ~/.config/hypr/hyprland/scripts/workspace_action.sh
+fi
